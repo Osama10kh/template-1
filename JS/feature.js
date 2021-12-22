3b5e31b8a9e9022e74a127153feff46159653fc7
Osama Khader <osama10kh@gmail.com>
2021-12-22 16:35:24 +0200

    add template-1

---
+++ JS/feature.js
@@ -0,0 +1,19 @@
+let icons = ["fa fa-magic", "fa fa-coffee", "fab fa-amazon", "fa fa-diamond", "fa fa-user", "fa fa-phone"];
+
+function creatEle() {
+    for (let i = 0; i < icons.length; i++) {
+        document.write(`
+        <div class = "feature-cont">
+        <h3>feature</h3>
+        <i class="${icons[i]}"></i>
+        <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        `)
+    }
+    return "";
+};
+document.write(`<section class="feature-sec centered">`);
+document.write(`${creatEle()}`);
+document.write(`</section>`);
+
+
